8ee87123f74630752a965ccc5c3a016ddff4bb25
fodorgabor <127303784+fodorgabor@users.noreply.github.com>
2024-03-28 14:25:01 +0100

    Kesz

--- Waiter.py
+++ Waiter.py
@@ -34,7 +34,29 @@ def osszegzes(o):
     osszeg=sum(o)+8.23  
     return osszeg
   
-def kiir(ossz,b,d,f,h):
+def eldontes2(o):
+    i=0
+    volt=False
+    while o[i]<=10 and i<len(o):
+        i+=1
+
+    if i<len(o):
+        volt=True
+
+    return i+1,volt
+
+def eldontes3(o):
+    i=0
+    volt=False
+    while i<len(o) and o[i]%5!=0:
+        i+=1
+
+    if i<len(o):
+        volt=True
+
+    return volt
+
+def kiir(ossz,b,d,f,h,j,j_volt_e,l):
     print(ossz)
     if b:
         print("Volt olyan, hogy a pincér valamit vett.")
@@ -43,6 +65,15 @@ def kiir(ossz,b,d,f,h):
     print(f"{d} alkalommal kapot biztosan penny-t is.")
     print(f"{f} esetben kapott legalább öt fontot.")
     print(f"Ha az óra elején már volt 8 font 23 penny a tárcájában, {h} pénz volt benne az óra végén.")
+    if j_volt_e:
+        print(f"A {j}. vendég volt az élső aki 10 fontnál többet fizetett.")
+    else:
+        print("Nem volt olyan vendég aki 10 fontnál többet fizetett.")
+    if l:
+        print("Volt olyan vendég, akinek módjában állt csupa ötfontossal kiegyenlíteni a számlát.")
+    else:
+        print("Nem volt olyan vendég, akinek módjában állt csupa ötfontossal kiegyenlíteni a számlát.")
+
 #Főprogram
 #Input
 osszegek=beker()
@@ -52,7 +83,8 @@ b=eldontes(osszegek)
 d=megszamolas(osszegek)
 f=megszamolas2(osszegek)
 h=osszegzes(osszegek)
-j=1
-l=1
+j,j_volt_e=eldontes2(osszegek)
+l=eldontes3(osszegek)
+
 #Output
-kiir(osszegek,b,d,f,h)
+kiir(osszegek,b,d,f,h,j,j_volt_e,l)
