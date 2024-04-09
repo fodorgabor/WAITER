14573cfde0fba59c01ca1dd7331f378a664bc64e
fodorgabor <127303784+fodorgabor@users.noreply.github.com>
2024-04-09 22:34:47 +0200

    Update Waiter.py

--- Waiter.py
+++ Waiter.py
@@ -3,7 +3,7 @@ def beker():
     lista=[]
     with open ("adatok.txt","r",encoding="UTF-8") as fm:
         for sor in fm:
-            lista.append(sor)
+            lista.append(float(sor.strip()))
     return lista
 
 def osszegez1(l):
