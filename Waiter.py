d7d2e1cc606fddf4c27abee32e64b8c56df8b193
fodorgabor <127303784+fodorgabor@users.noreply.github.com>
2024-04-09 22:32:30 +0200

    Update Waiter.py

--- Waiter.py
+++ Waiter.py
@@ -1,4 +1,11 @@
 #Alprogramok
+def beker():
+    lista=[]
+    with open ("adatok.txt","r",encoding="UTF-8") as fm:
+        for sor in fm:
+            lista.append(sor)
+    return lista
+
 def osszegez1(l):
     szum=0
     for elem in l:
